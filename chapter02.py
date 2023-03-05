bf0c7559fbe92845bff197f01108900dd4ccb2fb
Arne Jochens <A.Jochens@yahoo.com>
2023-03-05 22:15:31 +0100

    Calculating the output of one layer of neurons.

--- chapter02.py
+++ chapter02.py
@@ -4,8 +4,14 @@
 # Coding Our First Neurons
 
 inputs = [1, 2, 3, 2.5]
-weights = [0.2, 0.8, -0.5, 1.0]
-bias = 2
 
-output = bias + sum(i * w for i, w, in zip(inputs, weights))
-print(output)
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2, 3, 0.5]
+
+layer_outputs = [sum(i * w for i, w, in zip(inputs, weights[n])) + biases[n] 
+                 for n in range(3)]
+
+print(layer_outputs)
